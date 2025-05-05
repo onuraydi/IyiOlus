41d5b79a40b70b8201cbe0b46e12e78efd921933
Onur Aydı <onuraydi@outlook.com>
2025-05-05 14:01:38 +0300

    Generik repository operasonları eklendi.

---
+++ IyiOlus.Core/Repositories/Pagination/Paginate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IyiOlus.Core.Repositories.Pagination
+{
+    public class Paginate<T>
+    {
+        public IList<T> Items { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }   
+        public int TotalPages { get; set; }
+        public int TotalItems { get; set; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+
+        public Paginate()
+        {
+            Items = new List<T>();
+        }
+
+        public Paginate(IList<T> items, int totalItems, PaginationParams paginationParams)
+        {
+            Items = items;
+            TotalItems = totalItems;
+            PageNumber = paginationParams.PageNumber;
+            PageSize = paginationParams.PageSize;
+            TotalPages = (int)Math.Ceiling(TotalItems / (double)PageSize);
+        }
+    }
+}
