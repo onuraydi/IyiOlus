b5c5fe7c25e04b8fd6f8f1c6866d343ccbe7605f
Onur Aydı <onuraydi@outlook.com>
2025-05-17 14:25:23 +0300

    servis registirationlar yapıldı

--- IyiOlus.Application/ApplicationServiceRegistration.cs
+++ IyiOlus.Application/ApplicationServiceRegistration.cs
@@ -1,7 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using IyiOlus.Application.Features.Contacts.Rules;
+using IyiOlus.Application.Features.DailyMoods.Rules;
+using IyiOlus.Application.Features.ProfileTypes.Rules;
+using IyiOlus.Application.Features.Questions.Rules;
+using IyiOlus.Application.Features.Settings.Rules;
+using IyiOlus.Application.Features.UserAccounts.Rules;
+using IyiOlus.Application.Features.UserProfiles.Rules;
+using IyiOlus.Application.Features.Users.Rules;
+using IyiOlus.Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +22,24 @@ namespace IyiOlus.Application
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddSingleton<HttpExceptionHandler>();
+
+            services.AddScoped<ContactBusinessRules>();
+            services.AddScoped<DailyMoodBusinessRules>();
+            services.AddScoped<ProfileTypeBusinessRules>();
+            services.AddScoped<QuestionBusinessRules>();
+            services.AddScoped<SettingBusinessRules>();
+            services.AddScoped<UserAccountBusinessRules>();
+            services.AddScoped<UserProfileBusinessRules>();
+            services.AddScoped<UserBusinessRules>();
 
             return services;
         }
